61c3d008b5325257fbf40610b51f802b47ec6a2f
Gerardo Arjona <gerardo.arj15@gmail.com>
2020-12-06 21:10:47 -0600

    add calendar validation for only 17 or plus years old

--- app/src/main/java/com/example/androidworkshopapp1/DatePickerFragment.java
+++ app/src/main/java/com/example/androidworkshopapp1/DatePickerFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.DialogFragment;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 //Date Picker
 public class DatePickerFragment extends DialogFragment {
@@ -19,12 +20,16 @@ public class DatePickerFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
+        int year = c.get(Calendar.YEAR) - 17;
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), (MainActivity)getActivity(), year, month, day);
+        DatePickerDialog dialog = new DatePickerDialog(getActivity(), (MainActivity)getActivity(), year, month, day);
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.YEAR, today.get(Calendar.YEAR) - 17);
+        dialog.getDatePicker().setMaxDate(today.getTimeInMillis());
+        return dialog;
     }
 
 }
